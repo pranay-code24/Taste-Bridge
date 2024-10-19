e7bd1f1f791641661350659a76dab194ecf00e43
Pranay <pranaygumashta@gmail.com>
2024-10-19 23:31:49 +0530

    gap modified again

--- src/auth/VerifyEmail.tsx
+++ src/auth/VerifyEmail.tsx
@@ -17,7 +17,7 @@ const VerifyEmail = () => {
     }
     return (
         <div className="flex items-center justify-center h-screen w-full">
-            <div className="p-8 rounded-md w-full max-w-md flex flex-col gap border border-gray-200">
+            <div className="p-8 rounded-md w-full max-w-md flex flex-col gap-10 border border-gray-200">
                 <div className="text-center">
                     <h1 className="font-extrabold text-2xl">Verify your email</h1>
                     <p className="text-sm text-gray-600">Enter the 6 digit code sent to your email address</p>

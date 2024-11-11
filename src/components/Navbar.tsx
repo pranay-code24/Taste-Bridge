217aac317b0af0c9037bb3726bb432b4561e5e6e
Pranay <pranaygumashta@gmail.com>
2024-11-11 23:41:21 +0530

    Building Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom"
 import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "./ui/menubar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Loader2, Moon, ShoppingCart, Sun } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Input } from "./ui/input";
 
 const Navbar = () => {
     const admin = true;
+    const loading = false;
     return (
         <div className="max-w-7xl mx-auto">
             <div className="flex items-center justify-between h-14">
@@ -38,9 +45,92 @@ const Navbar = () => {
                         )
                     }
                 </div>
+                <div className="flex items-center gap-4">
+                    <div>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="outline" size="icon">
+                                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                                <span className="sr-only">Toggle theme</span>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuItem>
+                                    Light
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    Dark
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                    <Link to="/cart" className="relative cursor-pointer">
+                        <ShoppingCart/>
+                        <Button size={'icon'} className="absolute -inset-y-3 left-2 text-xs rounded-full h-4 w-4 bg-red-500 hover:bg-red-500">1</Button>
+                    </Link>
+                    <div>
+                        <Avatar>
+                            <AvatarImage/>
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </div>
+                    <div>
+                        {
+                            loading ? (
+                                <Button className="bg-orange hover:bg-hoverOrange">
+                                    <Loader2 className="mr-2 h-4 w-4 animate-spin"/>
+                                    Please wait
+                                </Button>
+                            ) : (
+                                <Button className="bg-orange hover:bg-hoverOrange">Logout</Button>
+                            )
+                        }
+                    </div>
+                </div>
+                <div>
+                    {/* Mobile Responsive */}
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
+
+const MobileNavbar = () => {
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="outline">Open</Button>
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Edit profile</SheetTitle>
+                    <SheetDescription>
+                        Make changes to your profile here. Click save when you're done.
+                    </SheetDescription>
+                </SheetHeader>
+                <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                    {/* <Label htmlFor="name" className="text-right">
+                    Name
+                    </Label>
+                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                    Username
+                    </Label> */}
+                    <Input id="username" value="@peduarte" className="col-span-3" />
+                </div>
+                </div>
+                <SheetFooter>
+                    <SheetClose asChild>
+                        <Button type="submit">Save changes</Button>
+                    </SheetClose>
+                </SheetFooter>
+            </SheetContent>
+        </Sheet>
+    )
+}
